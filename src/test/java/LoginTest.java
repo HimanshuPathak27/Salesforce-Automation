d898427a4e66aa804ad23a1508bb4c9299be2dab
Himanshu Pathak <137168075+HimanshuPathak27@users.noreply.github.com>
2024-08-17 10:40:52 +0530

    Un-necessary 'App Launcher' code removed from Login test

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -14,15 +14,11 @@ public class LoginTest {
         driver.manage().window().maximize();
 
         driver.get("https://login.salesforce.com/");
+       
         driver.findElement(By.xpath(locators.usernameXpath)).sendKeys("himanshupathak2910@resilient-raccoon-k4buyy.com");
         driver.findElement(By.xpath(locators.passwordXpath)).sendKeys("HomoSapiens@1998");
         driver.findElement(By.xpath(locators.loginButtonXpath)).click();
-
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
-
-        driver.findElement(By.xpath(locators.appLauncher)).click();
-        driver.findElement(By.xpath(locators.searchBar_AppLauncher)).sendKeys("Sales");
-        driver.findElement(By.xpath(locators.salesApp_AppLauncher)).click();
-        //driver.quit();
+       
+        driver.quit();
     }
 }
